e9a12bd9c77d0079480fadc23f34ef41c53fdd2b
Jim Schyvens <jim.schyvens@bignited.be>
2023-06-20 13:36:01 +0200

    example project

---
+++ src/test/java/com/Bignited/AppTest.java
@@ -0,0 +1,18 @@
+package com.Bignited;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    //example
+    @Test
+    public void example() {
+        assertTrue( true );
+    }
+}
